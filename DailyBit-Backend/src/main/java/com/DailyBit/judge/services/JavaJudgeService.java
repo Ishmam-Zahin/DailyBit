6a58c1de1f55d692f9315a622009e5cf65c0be19
ishmam-zahin <ishmamzahin404@gmail.com>
2025-09-05 14:36:15 +0600

    updated judge java

--- DailyBit-Backend/src/main/java/com/DailyBit/judge/services/JavaJudgeService.java
+++ DailyBit-Backend/src/main/java/com/DailyBit/judge/services/JavaJudgeService.java
@@ -24,7 +24,7 @@ public class JavaJudgeService {
         }
     }
 
-    public ProcessBuilder getCompileProcessBuilder(Path dir){
+    private ProcessBuilder getCompileProcessBuilder(Path dir){
         ProcessBuilder pb = new ProcessBuilder(
                 "docker", "run", "--rm",
                 "-v", dir + ":/home", javaImageName,
@@ -35,7 +35,7 @@ public class JavaJudgeService {
         return  pb;
     }
 
-    public ProcessBuilder getExecuteProcessBuilder(Path dir, int ulimit, int timeout){
+    private ProcessBuilder getExecuteProcessBuilder(Path dir, int ulimit, int timeout){
         ProcessBuilder pb = new ProcessBuilder(
                 "docker", "run", "--rm",
                 "-v", dir + ":/home", javaImageName,
@@ -46,58 +46,34 @@ public class JavaJudgeService {
         return  pb;
     }
 
-    public String executeJava(String code) {
-        Path javaBase = this.javaBase;
-        Path tmpDir = null;
+    private Path createTempJavaFile(String code) throws IOException {
+        Path tmpDir = Files.createTempDirectory(this.javaBase, "temp_");
+        Path tmpJavaFile = tmpDir.resolve(this.javaFileName + ".java");
+        Files.writeString(tmpJavaFile, code);
+
+        return tmpDir;
+    }
+
+    public boolean compileJava(Path tmpDir) throws IOException, InterruptedException {
+        boolean compiled = true;
+        ProcessBuilder compilePb = this.getCompileProcessBuilder(tmpDir);
+        Process p = compilePb.start();
+        int exitCode = p.waitFor();
+        if(exitCode != 0){
+            compiled = false;
+        }
+
+        return compiled;
+    }
+
+    public String runJava(Path tmpDir) throws IOException, InterruptedException {
         String result;
-        String cleanUpError = null;
 
-        try{
-            tmpDir = Files.createTempDirectory(javaBase, "temp_");
-            Path tmpJavaFile = tmpDir.resolve("Solution.java");
-            Files.writeString(tmpJavaFile, code);
+        ProcessBuilder runPb = this.getExecuteProcessBuilder(tmpDir, 2, 5);
+        Process p = runPb.start();
+        result = new String(p.getInputStream().readAllBytes());
+        int exitCode = p.waitFor();
 
-            ProcessBuilder compilePb = this.getCompileProcessBuilder(tmpDir);
-            Process p = compilePb.start();
-            String compileError = new String(p.getInputStream().readAllBytes());
-            int exitCode = p.waitFor();
-            if(exitCode != 0){
-                throw new JudgeException(compileError);
-            }
-            ProcessBuilder runPb = this.getExecuteProcessBuilder(tmpDir, 2, 5);
-            Process p2 = runPb.start();
-            String output = new String(p2.getInputStream().readAllBytes());
-            int exitCode2 = p2.waitFor();
-            if(exitCode2 != 0){
-                if(exitCode2 == 124 || exitCode2 == 137){
-                    output = "Time Limit Exceeded";
-                }
-                throw new JudgeException(output);
-            }
-
-            result = output;
-        }
-        catch (Exception e){
-            result = e.getMessage();
-        }
-        finally {
-            if (tmpDir != null) {
-                try (var paths = Files.walk(tmpDir)) {
-                    paths.sorted(Comparator.reverseOrder())
-                    .forEach(path -> {
-                        try {
-                            Files.delete(path);
-                        } catch (IOException e) {
-                            throw new RuntimeException(e);
-                        }
-                    });
-                }
-                catch (Exception e){
-                    cleanUpError = e.getMessage();
-                    System.out.println("cleanUpError: " + cleanUpError);
-                }
-            }
-        }
         return result;
     }
 }
