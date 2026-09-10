046995f478a55f9a22f3d005d4706b18f11e98c9
zahin <ishmamzahin404@gmail.com>
2026-09-10 12:35:05 +0600

    changes

--- dailybit_chatbot/main.py
+++ dailybit_chatbot/main.py
@@ -15,8 +15,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        os.getenv("FRONTEND_URL"),
         "http://localhost:3000",
-        "http://127.0.0.1:3000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
